9f4135fc8675482f2edfe7adf294dbf150f77b3e
Tamim Sookoor <tamimsookoor@gmail.com>
2014-12-13 11:20:28 -0500

    print out wedding numbers and use english numbers in beer on the wall

--- beer_on_the_wall.rb
+++ beer_on_the_wall.rb
@@ -1,13 +1,15 @@
-count = 99
+load 'englishNumber.rb'
+
+count = 9999
 
 while count > 0
   if count > 1
-    puts "#{count} bottles of beer on the wall, #{count} bottles of beer."
+    puts "#{englishNumber(count).capitalize} bottles of beer on the wall, #{englishNumber(count)} bottles of beer."
     count -= 1
-    puts "Take one down and pass it around, #{count} bottles of beer on the wall."
+    puts "Take one down and pass it around, #{englishNumber(count)} bottles of beer on the wall."
     puts ""
   else
-    puts "#{count} bottle of beer on the wall, #{count} bottle of beer."
+    puts "#{englishNumber(count).capitalize} bottle of beer on the wall, #{englishNumber(count)} bottle of beer."
     count -= 1
     puts "Take one down and pass it around, no more bottles of beer on the wall."
     puts ""

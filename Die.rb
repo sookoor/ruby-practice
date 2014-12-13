0978e6672b9a14853e63e0131a9347a2787a98bb
Tamim Sookoor <tamimsookoor@gmail.com>
2014-12-13 12:08:40 -0500

    Models a die

---
+++ Die.rb
@@ -0,0 +1,30 @@
+class Die
+
+  def initialize
+    # I'll just roll the die, though we could do something else if we
+    # wanted to, like setting the die with 6 showing.
+    roll
+  end
+
+  def roll
+    @numberShowing = 1 + rand(6)
+  end
+  
+  def showing
+    @numberShowing
+  end
+
+  def cheat(number)
+    @numberShowing = number
+  end
+
+end
+
+die = Die.new
+puts "Initialized: " + die.showing.to_s
+die.cheat(6)
+puts "Set to 6: " + die.showing.to_s
+die.roll
+puts "Rolled: " + die.showing.to_s
+die.cheat(3)
+puts "Set to 3: " + die.showing.to_s
